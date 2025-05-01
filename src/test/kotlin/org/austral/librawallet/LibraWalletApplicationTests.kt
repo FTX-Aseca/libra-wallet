cfbbca1e18aebe57613bed70979be3d97d07a8fe
FranCalveyra <franciscocalveyra24@gmail.com>
2025-05-01 17:19:06 -0300

    Initial Commit

---
+++ src/test/kotlin/org/austral/librawallet/LibraWalletApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.austral.librawallet
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LibraWalletApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
