6b60685d3c19877053b5eb2698ee88c22359e252
tomas <tomasrolb@gmail.com>
2025-05-29 22:15:15 -0300

    feat(testing): Add DirtiesContext annotation
    Add annotation to let Spring's context close before test container is turned down

--- src/test/kotlin/org/austral/librawallet/account/TopUpControllerIntegrationTest.kt
+++ src/test/kotlin/org/austral/librawallet/account/TopUpControllerIntegrationTest.kt
@@ -16,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
+import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
@@ -26,6 +27,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 class TopUpControllerIntegrationTest : IntegrationTestBase() {
 
     @Autowired
--- src/test/kotlin/org/austral/librawallet/auth/AuthControllerTests.kt
+++ src/test/kotlin/org/austral/librawallet/auth/AuthControllerTests.kt
@@ -15,6 +15,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
@@ -24,6 +25,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 class AuthControllerTests : IntegrationTestBase() {
 
     @Autowired
