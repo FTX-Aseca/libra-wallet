c302b3a20c70d7b1cedb3f8f6d2b829ae23b5dfb
FranCalveyra <franciscocalveyra24@gmail.com>
2025-06-12 19:27:22 -0300

    fix(test & config): Changed application.properties and fixed DataInitializer condition

--- src/main/kotlin/org/austral/librawallet/config/DataInitializer.kt
+++ src/main/kotlin/org/austral/librawallet/config/DataInitializer.kt
@@ -18,32 +18,28 @@ class DataInitializer(
     private val passwordEncoder: PasswordEncoder,
 ) : ApplicationRunner {
     override fun run(args: ApplicationArguments?) {
-        if (userRepository.count() == 0L) {
-            val user1 = userRepository.save(
-                User(email = "alice@example.com", password = passwordEncoder.encode("Password1!")),
-            )
-            accountRepository.save(
-                Account(
-                    user = user1,
-                    balance = 10_000L,
-                    alias = "sunny.river.001",
-                    cvu = "0123456789012345678901",
-                ),
-            )
-            val user2 = userRepository.save(
-                User(email = "bob@example.com", password = passwordEncoder.encode("Password2!")),
-            )
-            accountRepository.save(
-                Account(
-                    user = user2,
-                    balance = 5_000L,
-                    alias = "happy.mountain.002",
-                    cvu = "1098765432109876543210",
-                ),
-            )
-            println("DataInitializer: Data initialized")
-        } else {
-            println("DataInitializer: Data already initialized")
-        }
+        val user1 = userRepository.save(
+            User(email = "alice@example.com", password = passwordEncoder.encode("Password1!")),
+        )
+        accountRepository.save(
+            Account(
+                user = user1,
+                balance = 10_000L,
+                alias = "sunny.river.001",
+                cvu = "0123456789012345678901",
+            ),
+        )
+        val user2 = userRepository.save(
+            User(email = "bob@example.com", password = passwordEncoder.encode("Password2!")),
+        )
+        accountRepository.save(
+            Account(
+                user = user2,
+                balance = 5_000L,
+                alias = "happy.mountain.002",
+                cvu = "1098765432109876543210",
+            ),
+        )
+        println("DataInitializer: Data initialized")
     }
 }
